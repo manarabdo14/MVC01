357b1e3dff4877342da2f1de0e7759cba12df3b9
Manar Abdo <mannarabdo14@gmail.com>
2024-09-14 16:21:45 +0300

    Add project files.

---
+++ MVC01/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AboutUS()
+        {
+            return View();
+        }
+        
+        public IActionResult Privacy()
+        {
+            return View();
+        }    
+        public IActionResult ContactUS()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC01/Controllers/MoviesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC01.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public void AddMovie()
+        {
+
+        }
+
+
+        //public ContentResult Index() {
+        //   ContentResult actionResult = new ContentResult();
+        //    actionResult.Content = "Hello Manar";
+        //    return actionResult;
+        //}
+        public ContentResult Index() {
+    
+            return  Content("Hello Manar");
+        }
+
+
+        public IActionResult Redirect() { 
+            return RedirectToAction(nameof(Index));
+        }
+        //public IActionResult Redirect() { 
+        //    RedirectResult redirectResult = new RedirectResult("https://localhost:44388/Movies");
+        //    return redirectResult;
+        //}
+
+
+        //public RedirectResult Redirect() { 
+        //    RedirectResult redirectResult = new RedirectResult("https://localhost:44388/Movies");
+        //    return redirectResult;
+        //}
+
+
+
+    }
+}
